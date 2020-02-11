17aa5bb1bb5d10c28be29b5c5bc28a3d9a64b5cb
jangsiye <skfnsid123@naver.com>
2020-02-11 15:25:26 +0900

    Update Order.java

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,43 @@
 package model;
 
 public class Order {
-
+	private String mid;
+	private int oid;
+	private String orderdate;
+	
+	public Order() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Order(String mid, int oid, String orderdate) {
+		super();
+		this.mid = mid;
+		this.oid = oid;
+		this.orderdate = orderdate;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order [mid=" + mid + ", oid=" + oid + ", orderdate=" + orderdate + "]";
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public String getOrderdate() {
+		return orderdate;
+	}
+	public void setOrderdate(String orderdate) {
+		this.orderdate = orderdate;
+	}
+	
+	
 }
