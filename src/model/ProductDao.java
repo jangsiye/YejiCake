eb5629ceb82b0744f7c1bf40a29df6624478045b
jangsiye <skfnsid123@naver.com>
2020-02-12 10:36:13 +0900

    Update ProductDao.java

--- src/model/ProductDao.java
+++ src/model/ProductDao.java
@@ -7,50 +7,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.oreilly.servlet.MultipartRequest;
+
 public class ProductDao extends SuperDao {
 
 	public ProductDao() {
 		
 	}
 	
-		public int InsertData(MultipartRequest mr) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql =" insert into products(pnum, pname, price, point, content, image, hit, stock)";
-		sql += " values(seqboard.nextval, ?, ?, ?, ?, ?, 0, ?) ";
-		int cnt = -1;
-		try {
-			conn = super.getConnection();
-			conn.setAutoCommit(false);
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, mr.getParameter("pname")); 
-			pstmt.setString(2, mr.getParameter("price"));
-			pstmt.setString(3, mr.getParameter("point"));
-			pstmt.setString(4, mr.getParameter("content"));
-			pstmt.setString(5, mr.getFilesystemName("image"));
-			pstmt.setString(6, mr.getParameter("stock"));
-			
-			cnt = pstmt.executeUpdate();
-		
-			conn.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			try {
-				conn.rollback();
-			} catch (SQLException e2) {
-				e2.printStackTrace();
-			}
-		} finally {
-			try {
-				if(pstmt != null) {pstmt.close();}
-				if(conn != null) {conn.close();}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-		return cnt;
-	}	public int InsertData(MultipartRequest mr) {
+	public int InsertData(MultipartRequest mr) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		String sql =" insert into products(pnum, pname, price, point, content, image, hit, stock)";
@@ -92,7 +57,7 @@ public class ProductDao extends SuperDao {
 	public int UpdateData(Product bean) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		String sql = " update boards set pname=?, price=?, point=?, content=?, image=?, hit=? ";
+		String sql = " update boards set pname=?, price=?, point=?, content=?, image=?, hit=?, stock=? ";
 		sql += " where pnum = ? ";
 		int cnt = -1;
 		
@@ -101,14 +66,15 @@ public class ProductDao extends SuperDao {
 			conn.setAutoCommit(false);
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setString(1, bean.getName());
+			pstmt.setString(1, bean.getPname());
 			pstmt.setInt(2, bean.getPrice());
 			pstmt.setInt(3, bean.getPoint());
 			pstmt.setString(4, bean.getContent());
 			pstmt.setString(5, bean.getImage());
 			pstmt.setInt(6, bean.getHit());
-			pstmt.setInt(7, bean.getNum());
-
+			pstmt.setInt(7, bean.getStock());
+			pstmt.setInt(8, bean.getPnum());
+			
 
 			cnt = pstmt.executeUpdate();
 			
@@ -131,7 +97,7 @@ public class ProductDao extends SuperDao {
 		return cnt;
 	}
 
-	public int DeleteData(int num) {
+	public int DeleteData(int pnum) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		String sql = " delete from products ";
@@ -143,7 +109,7 @@ public class ProductDao extends SuperDao {
 			conn.setAutoCommit(false);
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setInt(1, num);
+			pstmt.setInt(1, pnum);
 			
 			cnt = pstmt.executeUpdate();
 			
@@ -196,7 +162,7 @@ public class ProductDao extends SuperDao {
 		return cnt;
 	}
 
-	public Product SelectByPk(int num) {
+	public Product SelectByPk(int pnum) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -208,20 +174,21 @@ public class ProductDao extends SuperDao {
 			conn = super.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setInt(1, num);
+			pstmt.setInt(1, pnum);
 			
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
 				product = new Product();
 				
-				product.setName(rs.getString("pname"));
+				product.setPname(rs.getString("pname"));
 				product.setPrice(rs.getInt("price"));
 				product.setPoint(rs.getInt("point"));				
 				product.setContent(rs.getString("content"));
 				product.setImage(rs.getString("image"));
 				product.setHit(rs.getInt("hit"));
-				product.setNum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("stock"));
 
 			}
 			
@@ -256,13 +223,14 @@ public class ProductDao extends SuperDao {
 			while(rs.next()) {
 				Product product = new Product();
 				
-				product.setName(rs.getString("pname"));
+				product.setPname(rs.getString("pname"));
 				product.setPrice(rs.getInt("price"));
 				product.setPoint(rs.getInt("point"));				
 				product.setContent(rs.getString("content"));
 				product.setImage(rs.getString("image"));
 				product.setHit(rs.getInt("hit"));
-				product.setNum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("stock"));
 				
 				list.add(product);
 			}
@@ -284,8 +252,8 @@ public class ProductDao extends SuperDao {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = " select pnum, pname, price, point, content, image, hit";
-		sql += " from ( select pnum, pname, price, point, content, image, hit rank() over(order by hit desc) as ranking from products) ";
+		String sql = " select pnum, pname, price, point, content, image, hit, stock";
+		sql += " from ( select pnum, pname, price, point, content, image, hit, stock rank() over(order by hit desc) as ranking from products) ";
 		sql += " where ranking between ? and ? ";
 		
 		List<Product> list = new ArrayList<Product>();
@@ -302,13 +270,14 @@ public class ProductDao extends SuperDao {
 			while(rs.next()) {
 				Product product = new Product();
 				
-				product.setName(rs.getString("pname"));
+				product.setPname(rs.getString("pname"));
 				product.setPrice(rs.getInt("price"));
 				product.setPoint(rs.getInt("point"));				
 				product.setContent(rs.getString("content"));
 				product.setImage(rs.getString("image"));
 				product.setHit(rs.getInt("hit"));
-				product.setNum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("pnum"));
+				product.setPnum(rs.getInt("stock"));
 				
 				list.add(product);
 			}
