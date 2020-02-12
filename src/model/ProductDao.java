e5900f183a1475e59757283bdfaa15c2254954d8
berrycider <40747996+berrycider@users.noreply.github.com>
2020-02-12 10:29:47 +0900

    Update ProductDao.java

--- src/model/ProductDao.java
+++ src/model/ProductDao.java
@@ -50,43 +50,6 @@ public class ProductDao extends SuperDao {
 			}
 		}
 		return cnt;
-	}	public int InsertData(MultipartRequest mr) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql =" insert into products(pnum, pname, price, point, content, image, hit, stock)";
-		sql += " values(seqboard.nextval, ?, ?, ?, ?, ?, 0, ?) ";
-		int cnt = -1;
-		try {
-			conn = super.getConnection();
-			conn.setAutoCommit(false);
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, mr.getParameter("pname")); 
-			pstmt.setString(2, mr.getParameter("price"));
-			pstmt.setString(3, mr.getParameter("point"));
-			pstmt.setString(4, mr.getParameter("content"));
-			pstmt.setString(5, mr.getFilesystemName("image"));
-			pstmt.setString(6, mr.getParameter("stock"));
-			
-			cnt = pstmt.executeUpdate();
-		
-			conn.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			try {
-				conn.rollback();
-			} catch (SQLException e2) {
-				e2.printStackTrace();
-			}
-		} finally {
-			try {
-				if(pstmt != null) {pstmt.close();}
-				if(conn != null) {conn.close();}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-		return cnt;
 	}
 
 	public int UpdateData(Product bean) {
