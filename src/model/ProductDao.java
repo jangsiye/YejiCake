d4da181fae99dfc5b8d22f3e887adecb0607720f
jangsiye <skfnsid123@naver.com>
2020-02-13 12:23:44 +0900

    Update ProductDao.java

--- src/model/ProductDao.java
+++ src/model/ProductDao.java
@@ -15,7 +15,6 @@ public class ProductDao extends SuperDao {
 		
 	}
 	
-	
 	public int UpHit(int pnum) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -53,8 +52,8 @@ public class ProductDao extends SuperDao {
 	public int InsertData(MultipartRequest mr) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		String sql =" insert into products(pnum, pname, price, point, content, image, hit, stock)";
-		sql += " values(seqpnum.nextval, ?, ?, ?, ?, ?, 0, ?) ";
+		String sql =" insert into products(pnum, pname, price, point, content, image, hit, stock, image2, category)";
+		sql += " values(seqpnum.nextval, ?, ?, ?, ?, ?, 0, ?, ?, ?) ";
 		int cnt = -1;
 		try {
 			conn = super.getConnection();
@@ -67,6 +66,8 @@ public class ProductDao extends SuperDao {
 			pstmt.setString(4, mr.getParameter("content"));
 			pstmt.setString(5, mr.getFilesystemName("image"));
 			pstmt.setInt(6, Integer.parseInt(mr.getParameter("stock")));
+			pstmt.setString(7, mr.getFilesystemName("image2"));
+			pstmt.setInt(8, Integer.parseInt(mr.getParameter("category")));
 			
 			cnt = pstmt.executeUpdate();
 		
@@ -92,7 +93,7 @@ public class ProductDao extends SuperDao {
 	public int UpdateData(Product bean) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		String sql = " update products set pname=?, price=?, point=?, content=?, image=?, hit=?, stock=? ";
+		String sql = " update products set pname=?, price=?, point=?, content=?, image=?, hit=?, stock=?, image2=?, category=? ";
 		sql += " where pnum = ? ";
 		int cnt = -1;
 		
@@ -108,7 +109,9 @@ public class ProductDao extends SuperDao {
 			pstmt.setString(5, bean.getImage());
 			pstmt.setInt(6, bean.getHit());
 			pstmt.setInt(7, bean.getStock());
-			pstmt.setInt(8, bean.getPnum());
+			pstmt.setString(8, bean.getImage2());
+			pstmt.setInt(9, bean.getCategory());
+			pstmt.setInt(10, bean.getPnum());
 			
 
 			cnt = pstmt.executeUpdate();
@@ -198,6 +201,7 @@ public class ProductDao extends SuperDao {
 	}
 
 	public Product SelectByPk(int pnum) {
+		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -224,7 +228,8 @@ public class ProductDao extends SuperDao {
 				product.setHit(rs.getInt("hit"));
 				product.setPnum(rs.getInt("pnum"));
 				product.setStock(rs.getInt("stock"));
-
+				product.setCategory(rs.getInt("category"));
+				product.setImage2(rs.getString("image2"));
 			}
 			
 		} catch (Exception e) {
@@ -242,6 +247,7 @@ public class ProductDao extends SuperDao {
 		return product;
 	}
 
+	//상품 전체를 읽어오는 메소드
 	public List<Product> SelectAll() {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -266,6 +272,52 @@ public class ProductDao extends SuperDao {
 				product.setHit(rs.getInt("hit"));
 				product.setPnum(rs.getInt("pnum"));
 				product.setStock(rs.getInt("stock"));
+				product.setCategory(rs.getInt("category"));
+				product.setImage2(rs.getString("image2"));
+				
+				list.add(product);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {rs.close();}
+				if(pstmt != null) {pstmt.close();}
+				if(conn != null) {conn.close();}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	//상품 중 케이크만, hit가 높은 순으로 정렬해서 데이터 가져오는 메소드
+	public List<Product> SelectAll_Hit() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = " select * from products where category = 1 order by hit desc" ;
+				
+		List<Product> list = new ArrayList<Product>();
+		
+		try {
+			conn = super.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Product product = new Product();
+				
+				product.setPname(rs.getString("pname"));
+				product.setPrice(rs.getInt("price"));
+				product.setPoint(rs.getInt("point"));				
+				product.setContent(rs.getString("content"));
+				product.setImage(rs.getString("image"));
+				product.setHit(rs.getInt("hit"));
+				product.setPnum(rs.getInt("pnum"));
+				product.setStock(rs.getInt("stock"));
+				product.setCategory(rs.getInt("category"));
+				product.setImage2(rs.getString("image2"));
 				
 				list.add(product);
 			}
@@ -283,12 +335,13 @@ public class ProductDao extends SuperDao {
 		return list;
 	}
 
+	//페이징 처리도 해주면서 pnum이 큰(신메뉴 순)으로 정렬해주는 메소드
 	public List<Product> SelectAll(int beginRow, int endRow) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = " select pnum, pname, price, point, content, image, hit, stock";
-		sql += " from ( select pnum, pname, price, point, content, image, hit, stock rank() over(order by hit desc) as ranking from products) ";
+		String sql = " select pnum, pname, price, point, content, image, hit, stock, image2, category ";
+		sql += " from ( select pnum, pname, price, point, content, image, hit, stock, image2, category rank() over(order by pnum desc) as ranking from products) ";
 		sql += " where ranking between ? and ? ";
 		
 		List<Product> list = new ArrayList<Product>();
