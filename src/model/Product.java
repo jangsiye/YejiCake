33d55a4f6d9370ae2090ac48f74de034d0c3c625
jangsiye <skfnsid123@naver.com>
2020-02-13 12:23:06 +0900

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -9,12 +9,15 @@ public class Product {
 	private String image;
 	private int hit;
 	private int stock;
+	private int category;
+	private String image2;
 	
 	public Product() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Product(int pnum, String pname, int price, int point, String content, String image, int hit, int stock) {
+	public Product(int pnum, String pname, int price, int point, String content, String image, int hit, int stock,
+			int category, String image2) {
 		super();
 		this.pnum = pnum;
 		this.pname = pname;
@@ -24,15 +27,37 @@ public class Product {
 		this.image = image;
 		this.hit = hit;
 		this.stock = stock;
-
+		this.category = category;
+		this.image2 = image2;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Product [pnum=" + pnum + ", pname=" + pname + ", price=" + price + ", point=" + point + ", content="
-				+ content + ", image=" + image + ", hit=" + hit + ", stock=" + stock + "]";
+				+ content + ", image=" + image + ", hit=" + hit + ", stock=" + stock + ", category=" + category
+				+ ", image2=" + image2 + "]";
 	}
-	
+
+	public int getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+
+	public String getImage2() {
+		return image2;
+	}
+
+
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+
+
 	public int getStock() {
 		return stock;
 	}
