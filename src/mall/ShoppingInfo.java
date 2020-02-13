57b4cfa760742a8b0fca5910b2be1ee38446f9e2
jangsiye <skfnsid123@naver.com>
2020-02-13 14:37:41 +0900

    Update ShoppingInfo.java

--- src/mall/ShoppingInfo.java
+++ src/mall/ShoppingInfo.java
@@ -9,13 +9,14 @@ public class ShoppingInfo {
 	private int price;	//상품 가격
 	private String image;	//상품 이미지
 	private int point;	//포인트
+	private String remark;	//비고(요청사항)
 	
 	//생성자
 	public ShoppingInfo() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public ShoppingInfo(int pnum, String pname, int qty, int price, String image, int point) {
+	public ShoppingInfo(int pnum, String pname, int qty, int price, String image, int point, String remark) {
 		super();
 		this.pnum = pnum;
 		this.pname = pname;
@@ -23,16 +24,23 @@ public class ShoppingInfo {
 		this.price = price;
 		this.image = image;
 		this.point = point;
+		this.remark = remark;
 	}
 	
 	//toString 오버라이딩
 	@Override
 	public String toString() {
 		return "ShoppingInfo [pnum=" + pnum + ", pname=" + pname + ", qty=" + qty + ", price=" + price + ", image="
-				+ image + ", point=" + point + "]";
+				+ image + ", point=" + point + ", remark=" + remark + "]";
 	}
 
 	//getter setter
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
 	public int getPnum() {
 		return pnum;
 	}
