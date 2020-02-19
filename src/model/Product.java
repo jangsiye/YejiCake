24163d799b64183823f240e49e8a333f6056d8ab
jangsiye <skfnsid123@naver.com>
2020-02-19 22:43:12 +0900

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -11,13 +11,18 @@ public class Product {
 	private int stock;
 	private int category;
 	private String image2;
+	private String tag1;
+	private String tag2;
+	private String tag3;
 	
 	public Product() {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+
 	public Product(int pnum, String pname, int price, int point, String content, String image, int hit, int stock,
-			int category, String image2) {
+			int category, String image2, String tag1, String tag2, String tag3) {
 		super();
 		this.pnum = pnum;
 		this.pname = pname;
@@ -29,14 +34,57 @@ public class Product {
 		this.stock = stock;
 		this.category = category;
 		this.image2 = image2;
+		this.tag1 = tag1;
+		this.tag2 = tag2;
+		this.tag3 = tag3;
 	}
+
+
 
 	@Override
 	public String toString() {
 		return "Product [pnum=" + pnum + ", pname=" + pname + ", price=" + price + ", point=" + point + ", content="
 				+ content + ", image=" + image + ", hit=" + hit + ", stock=" + stock + ", category=" + category
-				+ ", image2=" + image2 + "]";
+				+ ", image2=" + image2 + ", tag1=" + tag1 + ", tag2=" + tag2 + ", tag3=" + tag3 + "]";
 	}
+
+
+
+	public String getTag1() {
+		return tag1;
+	}
+
+
+
+	public void setTag1(String tag1) {
+		this.tag1 = tag1;
+	}
+
+
+
+	public String getTag2() {
+		return tag2;
+	}
+
+
+
+	public void setTag2(String tag2) {
+		this.tag2 = tag2;
+	}
+
+
+
+	public String getTag3() {
+		return tag3;
+	}
+
+
+
+	public void setTag3(String tag3) {
+		this.tag3 = tag3;
+	}
+
+
 
 	public int getCategory() {
 		return category;
